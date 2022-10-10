8139973fae0a5a63468d87cad6e77bc824774a2a
Abdallah Bari <abdellah01win@gmail.com>
2022-10-10 11:27:05 +0100

    make categoryCard components and pass it props

---
+++ resources/js/Components/CategoryCard.vue
@@ -0,0 +1,62 @@
+<script setup>
+import Dropdown from '@/Components/Dropdown.vue';
+import DropdownLink from '@/Components/DropdownLink.vue';
+</script>
+<script>
+export default {
+    name: "CategoryCard",
+    components: {
+        Dropdown,
+        DropdownLink,
+    },
+    props: {
+        data: Array
+    }
+};
+</script>
+<template>
+    <div v-for="(item, index) in data">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg flex">
+            <div class="p-6 grow">
+                <div class="flex justify-between items-center mb-3">
+                    <div class=" font-semibold text-lg">{{item.title[0].toUpperCase() +
+                    item.title.slice(1)}}
+                    </div>
+                    <Dropdown align="right" width="48">
+                        <template #trigger>
+                            <img src="../../../storage/assets/dots.svg" alt="" class="w-6">
+                        </template>
+
+                        <template #content>
+                            <DropdownLink :href="route('categories/edit')" method="post" as="button">
+                                Edit
+                            </DropdownLink>
+                            <DropdownLink :href="route('categories/destroy')" method="post" as="button">
+                                Delete
+                            </DropdownLink>
+                        </template>
+                    </Dropdown>
+                </div>
+                <div class="flex flex-col gap-y-3">
+                    <div class="flex items-center justify-between">
+                        <div class="flex items-center gap-2">
+                            <div class="bg-gray-200 rounded-full p-3 grid" style="place-content: center;">
+                                <img src="../../../storage/assets/edit.svg" alt="edit" class="w-4">
+                            </div>
+                            <div>{{item.total}}</div>
+                        </div>
+                        <div class="flex items-center gap-2">
+                            <div class="bg-gray-200 rounded-full p-2 grid" style="place-content: center;">
+                                <img src="../../../storage/assets/target.svg" alt="edit" class="w-5">
+                            </div>
+                            <div>{{item.limit}}</div>
+                        </div>
+                    </div>
+                    <div class="flex justify-between items-center">
+                        <div>{{item.updated_at}}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- resources/js/Pages/Categories.vue
+++ resources/js/Pages/Categories.vue
@@ -2,8 +2,7 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head, Link, } from '@inertiajs/inertia-vue3';
 import CategoryForm from '@/Components/CategoryForm.vue';
-import Dropdown from '@/Components/Dropdown.vue';
-import DropdownLink from '@/Components/DropdownLink.vue';
+import CategoryCard from '@/Components/CategoryCard.vue';
 
 const close = () => {
     const formHolder = document.getElementById('formHolder');
@@ -15,8 +14,7 @@ import axios from 'axios';
 export default {
     components: {
         CategoryForm,
-        Dropdown,
-        DropdownLink
+        CategoryCard
     },
     data() {
         return {
@@ -29,7 +27,6 @@ export default {
         loadCategories() {
             axios.get('categories/list').then((response) => {
                 let categories = response.data
-                console.log(categories)
                 categories.forEach(element => {
                     if (element.type === "Expances") {
                         this.expances.push(element);
@@ -65,119 +62,19 @@ export default {
                 <div class="mb-6">
                     <h3 class="mb-2 font-semibold text-xl">Expances Categories</h3>
                     <div class="grid grid-cols-3 gap-8">
-                        <div v-for="(item, index) in expances">
-                            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg flex">
-                                <div class="p-6 grow">
-                                    <div class="mb-2 font-semibold text-lg">{{item.title[0].toUpperCase() +
-                                    item.title.slice(1)}}
-                                    </div>
-                                    <div class="flex flex-col gap-y-2">
-                                        <div class="flex justify-between items-center ">
-                                            <div>{{item.total}}</div>
-                                            <div>{{item.limit}}</div>
-                                        </div>
-                                        <div class="flex justify-between items-center">
-                                            <div>{{item.updated_at}}</div>
-                                            <div class="bg-gray-200 rounded-full p-3 grid"
-                                                style="place-content: center;">
-                                                <img src="../../../storage/assets/edit.svg" alt="edit" class="w-4">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        <CategoryCard :data="expances" />                  
                     </div>
                 </div>
                 <div class="mb-6">
                     <h3 class="mb-2 font-semibold text-xl">Income Categories</h3>
                     <div class="grid grid-cols-3 gap-8">
-                        <div v-for="(item, index) in income">
-                            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg flex">
-                                <div class="p-6 grow">
-                                <div class="flex justify-between items-center mb-3">
-                                    <div class=" font-semibold text-lg">{{item.title[0].toUpperCase() +
-                                    item.title.slice(1)}}
-                                    </div>
-                                    <Dropdown align="right" width="48">
-                                    <template #trigger>
-                                        <img src="../../../storage/assets/dots.svg" alt="" class="w-6">
-                                    </template>
-
-                                    <template #content>
-                                        <DropdownLink :href="route('categories/edit')" method="post" as="button">
-                                            Edit
-                                        </DropdownLink>
-                                        <DropdownLink :href="route('categories/destroy')" method="post" as="button">
-                                            Delete
-                                        </DropdownLink>
-                                    </template>
-                                </Dropdown>
-                                </div>
-                                    <div class="flex flex-col gap-y-3">
-                                    <div class="flex items-center justify-between">
-                                        <div class="flex items-center gap-2">
-                                            <div class="bg-gray-200 rounded-full p-3 grid"
-                                                style="place-content: center;">
-                                                <img src="../../../storage/assets/edit.svg" alt="edit" class="w-4">
-                                            </div>
-                                            <div>{{item.total}}</div>
-                                        </div>
-                                        <div class="flex items-center gap-2">
-                                            <div class="bg-gray-200 rounded-full p-2 grid"
-                                                style="place-content: center;">
-                                                <img src="../../../storage/assets/target.svg" alt="edit" class="w-5">
-                                            </div>
-                                            <div>{{item.limit}}</div>
-                                        </div>
-                                    </div>
-                                        <div class="flex justify-between items-center">
-                                            <div>{{item.updated_at}}</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        <CategoryCard :data="income" />
                     </div>
                 </div>
                 <div class="mb-6">
                     <h3 class="mb-2 font-semibold text-xl">Investment Categories</h3>
                     <div class="grid grid-cols-3 gap-8">
-                        <div v-for="(item, index) in investments">
-                            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg flex">
-                                <div class="p-6 grow">
-                                    <div class="mb-2 font-semibold text-lg">{{item.title[0].toUpperCase() +
-                                    item.title.slice(1)}}
-                                    </div>
-                                    <div class="flex flex-col gap-y-2">
-                                        <div class="flex justify-between items-center ">
-                                            <div>
-                                                <div class="bg-gray-200 rounded-full p-3 grid"
-                                                    style="place-content: center;">
-                                                    <img src="../../../storage/assets/edit.svg" alt="edit" class="w-4">
-                                                </div>
-                                                <div>{{item.total}}</div>
-                                            </div>
-                                            <div>
-                                                <div class="bg-gray-200 rounded-full p-3 grid"
-                                                    style="place-content: center;">
-                                                    <img src="../../../storage/assets/target.svg" alt="edit"
-                                                        class="w-4">
-                                                </div>
-                                                <div>{{item.limit}}</div>
-                                            </div>
-                                        </div>
-                                        <div class="flex justify-between items-center">
-                                            <div>{{item.updated_at}}</div>
-                                            <div class="bg-gray-200 rounded-full p-3 grid"
-                                                style="place-content: center;">
-                                                <img src="../../../storage/assets/edit.svg" alt="edit" class="w-4">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        <CategoryCard :data="investments" />                  
                     </div>
                 </div>
             </div>
