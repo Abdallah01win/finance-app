56d52fc959851fbea5c3d167187cffd9e5c9d1b8
Abdallah Bari <abdellah01win@gmail.com>
2023-03-05 14:20:46 +0100

    remove targets from user migration

--- resources/js/Pages/Analytics.vue
+++ resources/js/Pages/Analytics.vue
@@ -13,9 +13,12 @@ import { Head } from '@inertiajs/inertia-vue3';
 
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="grid grid-cols-3 gap-8">
-                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg flex my-8">d1</div>
-                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg flex my-8">d2</div>
-                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg flex my-8">d3</div>
+                    <div>
+                        Avrage weekly expances
+                    </div>
+                    <div>
+                        Avrage monthly expances
+                    </div>
                 </div>
             </div>
         </div>
